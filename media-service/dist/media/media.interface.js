77d01fa995a7efb63472630167ebb64fce23ce8a
Nguyễn Thế Đạt <nguyenthedat@MacBook-cua-Dat.local>
2024-08-30 20:53:17 +0700

    fix

--- media-service/dist/media/media.interface.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MediaResponse = void 0;
-class MediaResponse {
-}
-exports.MediaResponse = MediaResponse;
-//# sourceMappingURL=media.interface.js.map
--- media-service/dist/responseClass.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class ResponseData<D> {
-    data: D | D[];
-    status: number;
-    message: string;
-    constructor(data: D | D[], status: number, message: string);
-}
